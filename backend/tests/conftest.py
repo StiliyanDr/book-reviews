40852e70204da4a8d1e6ea41fdc26f35b1b4cb85
Stiliyan Dragnev <stiliyan_dragnev@yahoo.com>
2025-04-12 14:11:16 +0000

    Add tests for books dtl

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,9 +1,38 @@
 import pytest
 
 from bookreviews.inftrastructure.configuration.configuration import Configuration
+from bookreviews.models.book import Book
 from tests.utils.stubconfig import create_stub_config
 
 
 @pytest.fixture
 def stub_config() -> Configuration:
     return create_stub_config()
+
+
+@pytest.fixture
+def raw_book() -> dict[str, str | list[str] | None]:
+    return {
+        "Title": "The Catcher in the Rye",
+        "description": "A book about a boy",
+        "authors": ["J.D. Salinger"],
+        "image": "https://...",
+        "previewLink": "https://...",
+        "publisher": "Penguin",
+        "publishedDate": "1951",
+        "categories": ["Fiction"],
+        "ratingsCount": None,
+    }
+
+
+@pytest.fixture
+def book(raw_book: dict[str, str | list[str] | None]) -> Book:
+    return Book(title=raw_book["Title"],
+                description=raw_book["description"],
+                authors=raw_book["authors"],
+                image_url=raw_book["image"],
+                preview_url=raw_book["previewLink"],
+                publisher=raw_book["publisher"],
+                published_date=raw_book["publishedDate"],
+                categories=raw_book["categories"],
+                ratings_count=0)
---
+++ backend/tests/unit/dtls/test_books_dtl.py
@@ -0,0 +1,33 @@
+from unittest.mock import create_autospec
+
+import pytest
+
+from backend.bookreviews.connections.mongosession import MongoSession
+from backend.bookreviews.dtls.books_dtl import BooksDTL
+from backend.bookreviews.models.book import Book
+
+
+class TestGetAll:
+    @pytest.mark.asyncio
+    async def test_exceptions_propagate(self) -> None:
+        session = create_autospec(MongoSession)
+        dtl = BooksDTL(session)
+        session.find.side_effect = RuntimeError("Error!")
+
+        with pytest.raises(RuntimeError):
+            await dtl.get_all()
+
+        session.find.assert_called_with("books", limit=None)
+
+    @pytest.mark.asyncio
+    async def test_book_objects_are_returned(self, raw_book, book) -> None:
+        session = create_autospec(MongoSession)
+        dtl = BooksDTL(session)
+        session.find.return_value = [raw_book]
+
+        result = await dtl.get_all()
+
+        assert isinstance(result, list)
+        assert len(result) == 1
+        assert result[0] == book
+        session.find.assert_called_with("books", limit=None)
