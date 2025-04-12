f2689b95cd19eaa3b06fd0bbdf797dfdf12cf44e
Stiliyan Dragnev <stiliyan_dragnev@yahoo.com>
2025-04-12 13:24:04 +0000

    Add the book model

---
+++ backend/bookreviews/models/book.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Book(BaseModel):
+    title: str
+    description: Optional[str]
+    authors: list[str]
+    image_url: Optional[str]
+    preview_url: Optional[str]  
+    publisher: Optional[str]
+    published_date: Optional[str]
+    categories: list[str]
+    ratings_count: Optional[int]
+ 
