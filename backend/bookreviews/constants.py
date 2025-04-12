a71c926a6377e9207e6f0b89cbd2c07cbbf4ae21
Stiliyan Dragnev <stiliyan_dragnev@yahoo.com>
2025-04-12 13:26:03 +0000

    Add two enums

--- backend/bookreviews/constants.py
+++ backend/bookreviews/constants.py
@@ -3,3 +3,17 @@ import enum
 
 class SecretNames(enum.StrEnum):
     MONGO = "mongodb"
+
+
+class HTTPMethods(enum.StrEnum):
+    GET = "GET"
+    POST = "POST"
+    PUT = "PUT"
+    DELETE = "DELETE"
+    PATCH = "PATCH"
+
+
+class Landscape(enum.StrEnum):
+    DEV = "dev"
+    PROD = "prod"
+    QA = "qa"
